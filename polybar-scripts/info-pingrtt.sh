e1c31bf376b1748e7782f29de231d8ca1d7a7535
tmwatchanan <tmwatchanan@gmail.com>
2018-09-23 12:58:02 +0700

    Fix font awesome bug

--- polybar-scripts/info-pingrtt.sh
+++ polybar-scripts/info-pingrtt.sh
@@ -5,11 +5,11 @@ HOST=google.com
 rtt=$(ping $HOST -c 1 | sed -rn 's/.*time=([0-9]{1,})\.?[0-9]{0,} ms.*/\1/p')
 
 if [ "$rtt" -lt 50 ]; then
-    icon="%{F#3cb703}#%{F-}"
+    icon="%{F#3cb703}%{F-}"
 elif [ "$rtt" -lt 150 ]; then
-    icon="%{F#f9dd04}#%{F-}"
+    icon="%{F#f9dd04}%{F-}"
 else
-    icon="%{F#d60606}#%{F-}"
+    icon="%{F#d60606}%{F-}"
 fi
 
 echo "$icon $rtt ms"
