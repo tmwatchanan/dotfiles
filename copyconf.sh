395de4b44033668db925dc6bb809e52791a63e97
tmwatchanan <tmwatchanan@gmail.com>
2018-09-23 17:18:06 +0700

    Add keys binding, fix typo for launching compton

--- copyconf.sh
+++ copyconf.sh
@@ -3,6 +3,7 @@
 cp $HOME/.Xdefaults $HOME/dotfiles/
 cp $HOME/.bashrc $HOME/dotfiles/
 cp $HOME/.xinitrc $HOME/dotfiles/
+cp $HOME/.xbindkeysrc $HOME/dotfiles/
 
 cp $HOME/.config/compton.conf $HOME/dotfiles/.config/
 cp $HOME/.config/i3/config $HOME/dotfiles/.config/i3/
