e09fab9e3690b176d0243581a81479062762bceb
tmwatchanan <tmwatchanan@gmail.com>
2018-11-06 11:48:47 +0700

    Add backing up fonts

--- backup_conf.sh
+++ backup_conf.sh
@@ -17,6 +17,7 @@ cp $HOME/.config/ranger/rifle.conf $HOME/dotfiles/.config/ranger/
 cp $HOME/.config/ranger/scope.sh $HOME/dotfiles/.config/ranger/
 cp $HOME/.config/dunst/* $HOME/dotfiles/.config/dunst/
 
+mkdir -p $HOME/dotfiles/.fonts/ && cp $HOME/.fonts/* $_
 cp -r $HOME/.config/colors/* $HOME/dotfiles/.config/colors/
 rm -rf $HOME/dotfiles/.wallpaper && cp -r $HOME/.wallpaper $HOME/dotfiles/
 
