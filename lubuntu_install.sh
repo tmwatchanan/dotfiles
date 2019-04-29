1d416140e2c2020cabd366c7f39716ac7f0f29f5
tmwatchanan <tmwatchanan@gmail.com>
2019-04-29 23:48:31 +0700

    Add tmux installation

--- lubuntu_install.sh
+++ lubuntu_install.sh
@@ -32,3 +32,8 @@ ln -s ~/.local/kitty.app/bin/kitty ~/bin/
 cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
 # Update the path to the kitty icon in the kitty.desktop file
 sed -i "s/Icon\=kitty/Icon\=\/home\/$USER\/.local\/kitty.app\/share\/icons\/hicolor\/256x256\/apps\/kitty.png/g" ~/.local/share/applications/kitty.desktop
+
+# tmux
+sudo apt install -y tmux
+# tpm (tmux plugins)
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
