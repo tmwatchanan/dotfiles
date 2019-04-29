117da3fc004b838bd54c4bbe798fc69056c8399d
tmwatchanan <tmwatchanan@gmail.com>
2019-04-29 23:42:30 +0700

    Add Lubuntu installation script

---
+++ lubuntu_install.sh
@@ -0,0 +1,34 @@
+sudo apt update
+
+# development
+sudo apt install -y curl git build-essential
+
+# zsh shell sudo apt install -y zsh
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# oh-my-zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# neovim
+sudo apt install -y neovim
+# vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# install neovim's plugins
+nvim +'PlugInstall --sync' +qa
+
+# install miniconda3
+# for neovim python compatible
+pip install pynvim
+
+# kitty
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+sudo apt install -y libdbus-1-dev libxcursor-dev libxrandr-dev libxi-dev libxinerama-dev libgl1-mesa-dev libxkbcommon-x11-dev libfontconfig-dev and libpython-dev
+# Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
+# your PATH)
+ln -s ~/.local/kitty.app/bin/kitty ~/bin/
+# Place the kitty.desktop file somewhere it can be found by the OS
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications
+# Update the path to the kitty icon in the kitty.desktop file
+sed -i "s/Icon\=kitty/Icon\=\/home\/$USER\/.local\/kitty.app\/share\/icons\/hicolor\/256x256\/apps\/kitty.png/g" ~/.local/share/applications/kitty.desktop
