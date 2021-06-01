11a8c511950ff4d26d7087d6d6ab18a1c3683935
tmwatchanan <tmwatchanan@gmail.com>
2021-06-01 17:46:01 -0500

    feat(nvim): add sumneko_lua language server and refactor lspconfig

--- .config/nvim/lua/plug/nvim-lspconfig.lua
+++ .config/nvim/lua/plug/nvim-lspconfig.lua
@@ -1,3 +1,14 @@
+--[[
+npm install -g pyright
+npm install -g vscode-html-languageserver-bin
+npm install -g vscode-css-languageserver-bin
+npm install -g typescript typescript-language-server
+npm install -g vscode-json-languageserver
+npm install -g yaml-language-server
+go get github.com/lighttiger2505/sqls
+npm install -g dockerfile-language-server-nodejs
+--]]
+
 --Show function signature
 local function on_attach(client)
     local cfg = {
@@ -10,13 +21,14 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-
---[[ Python -------------------------------------------------------------------
-npm install -g pyright
---]]
-require'lspconfig'.pyright.setup {
-    on_attach = on_attach,
-}
+local nvim_lsp = require('lspconfig')
+local servers = {'pyright', 'sumneko_lua', 'html', 'cssls', 'tsserver', 'jsonls', 'yamlls', 'sqls', 'dockerls'}
+for _, lsp in pairs(servers) do
+    nvim_lsp[lsp].setup {
+        on_attach = on_attach,
+        capabilities = capabilities,
+    }
+end
 
 --[[ Lua ----------------------------------------------------------------------
 sudo apt install -y ninja-build
@@ -29,57 +41,47 @@ compile/install.sh
 cd ../..
 ./3rd/luamake/luamake rebuild
 --]]
-require'lspconfig'.sumneko_lua.setup {
+local system_name
+if vim.fn.has("mac") == 1 then
+    system_name = "macOS"
+elseif vim.fn.has("unix") == 1 then
+    system_name = "Linux"
+elseif vim.fn.has('win32') == 1 then
+    system_name = "Windows"
+else
+    print("Unsupported system for sumneko")
+end
+
+local sumneko_root_path = "/home/mie/.config/nvim/language_servers/lua-language-server"
+local sumneko_binary = sumneko_root_path .. "/bin/Linux/lua-language-server"
+
+nvim_lsp.sumneko_lua.setup {
     on_attach = on_attach,
+    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+                -- Setup your lua path
+                path = vim.split(package.path, ';'),
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = {
+                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+                },
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
 }
 
---[[ HTML ---------------------------------------------------------------------
-npm install -g vscode-html-languageserver-bin
---]]
-require'lspconfig'.html.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
-
---[[ CSS ----------------------------------------------------------------------
-npm install -g vscode-css-languageserver-bin
---]]
-require'lspconfig'.cssls.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
-
---[[ TypeScript/JavaScript ----------------------------------------------------
-npm install -g typescript typescript-language-server
---]]
-require'lspconfig'.tsserver.setup {
-    on_attach = on_attach,
-}
-
---[[ JSON ---------------------------------------------------------------------
-npm install -g vscode-json-languageserver
---]]
-require'lspconfig'.jsonls.setup {
-    on_attach = on_attach,
-}
-
---[[ YAML ---------------------------------------------------------------------
-npm install -g yaml-language-server
-]]
-require'lspconfig'.yamlls.setup {
-    on_attach = on_attach,
-}
-
---[[ SQL ----------------------------------------------------------------------
-go get github.com/lighttiger2505/sqls
---]]
-require'lspconfig'.sqls.setup {
-    on_attach = on_attach,
-}
-
---[[ Docker -------------------------------------------------------------------
-npm install -g dockerfile-language-server-nodejs
---]]
-require'lspconfig'.dockerls.setup {
-    on_attach = on_attach,
-}
