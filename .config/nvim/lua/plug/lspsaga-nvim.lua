990723fb4ca9658fa521840a832de7e70ffaff24
tmwatchanan <tmwatchanan@gmail.com>
2021-06-01 12:46:58 -0500

    feat(nvim): change some key mappings for lsp

--- .config/nvim/lua/plug/lspsaga-nvim.lua
+++ .config/nvim/lua/plug/lspsaga-nvim.lua
@@ -12,5 +12,7 @@ local defaults = {
 }
 
 vim.api.nvim_set_keymap('n', 'gh', ':Lspsaga lsp_finder<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gD', ':Lspsaga preview_definition<CR>', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', 'gs', ':Lspsaga signature_help<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>ca', ':Lspsaga code_action<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<leader>ca', ':<C-U>Lspsaga range_code_action<CR>', { noremap = true, silent = true })
