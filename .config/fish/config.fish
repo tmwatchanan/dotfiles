87be5c6a5a1ab700fbe1c6307ba3aa71eec79201
tmwatchanan <tmwatchanan@users.noreply.github.com>
2024-08-19 14:12:00 +0700

    feat(fish): `p` function for navigating to project dir (`z`) and activate conda env

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -134,6 +134,14 @@ end
 
 alias ipy='ipython -i -c "%load_ext autoreload" "%autoreload 2"'
 
+function p
+    set dir_name $argv[1]
+    set env_name $dir_name
+    if test $argv[2]; set env_name $argv[2]; end
+	z $dir_name
+    conda activate $env_name
+end
+
 function vscp
 	v scp://$argv[1]//root/workspace/$argv[2]
 end
