91e6c4baa682889caacb911d4832f19318f792f5
tmwatchanan <tmwatchanan@users.noreply.github.com>
2024-12-28 22:15:59 +0700

    feat(yazi): changing current working directory when exiting Yazi (`Q` to not to do)

--- .config/fish/functions/fa.fish
+++
@@ -1,9 +0,0 @@
-function fa
-	set tmp (mktemp -t "yazi-cwd.XXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
-end
-
