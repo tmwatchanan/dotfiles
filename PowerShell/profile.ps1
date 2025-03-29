f6bfb8c01e8da973682f058fce56d39a1f32f65a
tmwatchanan <tmwatchanan@users.noreply.github.com>
2025-03-29 20:53:00 +0700

    feat(powershell): add `cfwt`, `lg.`, `lg.c`, `grep`

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -7,7 +7,21 @@ Set-Alias lg lazygit
 function reload { . $PROFILE }
 function cfp { nvim $PROFILE }
 
-function cfgit { nvim $env:USERPROFILE/.gitconfig }
+function cfgit { nvim $env:USERPROFILE\.gitconfig }
+function cfwt { nvim $dotfilesPath\.config\wezterm\theme.lua }
 
 function cd. { set-location $dotfilesPath }
 function cd.c { set-location $dotfilesConfigPath }
+function lg. {
+    Push-Location -EA Stop $dotfilesPath
+    lazygit
+    Pop-Location
+}
+function lg.c {
+    Push-Location -EA Stop $dotfilesConfigPath
+    lazygit
+    Pop-Location
+}
+
+Set-Alias grep findstr
+
