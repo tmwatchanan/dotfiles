33dd0b008382dd57883a0d98b9310f07149a4b4c
tmwatchanan <tmwatchanan@users.noreply.github.com>
2025-03-29 21:23:15 +0700

    feat(powershell): add more functions and aliases

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,17 +1,26 @@
-$dotfilesPath = "D:\dev\dotfiles"
-$dotfilesConfigPath = "D:\dev\dotfiles-config"
+$devPath = "D:\dev"
+$dotfilesPath = "$devPath\dotfiles"
+$dotfilesConfigPath = "$devPath\dotfiles-config"
 
 Set-Alias v nvim
 Set-Alias lg lazygit
+Set-Alias f yazi
 
-function reload { . $PROFILE }
+function reload {
+    Add-Type -AssemblyName System.Windows.Forms
+    [System.Windows.Forms.SendKeys]::SendWait(". $")
+    [System.Windows.Forms.SendKeys]::SendWait("PROFILE")
+    [System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+}
 function cfp { nvim $PROFILE }
 
 function cfgit { nvim $env:USERPROFILE\.gitconfig }
-function cfwt { nvim $dotfilesPath\.config\wezterm\theme.lua }
+function cfv { Set-Location $dotfilesConfigPath\nvim; nvim $dotfilesConfigPath\nvim\lua\config\keymaps.lua }
+function cfwt { Set-Location $dotfilesPath\.config\wezterm; nvim $dotfilesPath\.config\wezterm\theme.lua }
 
-function cd. { set-location $dotfilesPath }
-function cd.c { set-location $dotfilesConfigPath }
+function cdd { Set-Location $devPath }
+function cd. { Set-Location $dotfilesPath }
+function cd.c { Set-Location $dotfilesConfigPath }
 function lg. {
     Push-Location -EA Stop $dotfilesPath
     lazygit
@@ -22,6 +31,10 @@ function lg.c {
     lazygit
     Pop-Location
 }
+
+function admin { Start-Process -Verb RunAs wt.exe pwsh.exe }
+function cd- { cd - }
+function .. { cd .. }
 
 Set-Alias grep findstr
 
